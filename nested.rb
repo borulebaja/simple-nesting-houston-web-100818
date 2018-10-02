ea654f1e98881739d87cc027aa7a05c8096522c5
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2018-10-02 02:50:55 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -124,5 +124,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  programmer_hash[:grace_hopper][:known_for].push("Assembly")
+  programmer_hash[:dennis_ritchie][:languages].push("Assembly")
 end
