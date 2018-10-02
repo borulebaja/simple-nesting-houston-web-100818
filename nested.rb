be27cca34417563587a171a361cfcbce6e533797
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2018-10-02 02:59:12 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -104,7 +104,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-  programmer_hash[:grace_hopper][:known_for]
+  programmer_hash[:alan_kay][:known_for]
 end
 
 def adding_to_dennis
@@ -124,6 +124,6 @@ def adding_to_dennis
         }
      }
   programmer_hash[:dennis_ritchie][:languages].push["Assembly"]
-  programmer_hash
+
 end
   
